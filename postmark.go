b6f5787e6a17edd39cd2b196e44b788c6c213366
johndunne <john.dunne@epexcel.com>
2016-04-13 23:09:48 +0100

    Added batch mail upload

--- postmark.go
+++ postmark.go
@@ -5,10 +5,12 @@ import (
     "net/http"
     "bytes"
     "fmt"
+    "encoding/json"
+    "io/ioutil"
 )
 
 const emailUrl = "https://api.postmarkapp.com/email"
-//const batchUrl = "https://api.postmarkapp.com/email/batch"
+const batchUrl = "https://api.postmarkapp.com/email/batch"
 
 type Postmark struct {
     key string
@@ -38,7 +40,48 @@ func (p *Postmark) Send(m *Message)(* Response, error){
         case rsp.StatusCode == 401:
             return nil, fmt.Errorf("Missing of incorrect API key header")
         case rsp.StatusCode == 422:
-            return nil, fmt.Errorf("Malformed JSON or incorrect fields")
+            bytes,_:=ioutil.ReadAll(rsp.Body)
+            return nil, fmt.Errorf(string(bytes))
+        case rsp.StatusCode == 500:
+            return nil, fmt.Errorf("Postmark seems to be down!")
+    }
+
+    var body bytes.Buffer
+    _, err = io.Copy(&body, rsp.Body)
+    rsp.Body.Close()
+    if err != nil {
+        return nil, err
+    }
+
+    prsp, err := UnmarshalResponse([]byte(body.String()))
+    if err != nil {
+        return nil, err
+    }
+    return prsp, nil
+}
+
+func (p *Postmark) SendBatch(msgs []*Message)(* Response, error){
+
+    data, err := json.Marshal(msgs)
+    if err != nil {
+        return nil, err
+    }
+    postData := bytes.NewBuffer(data)
+    req, err := http.NewRequest("POST", batchUrl, postData)
+    req.Header.Set("Accept", "application/json")
+    req.Header.Set("Content-Type", "application/json")
+    req.Header.Set("X-Postmark-Server-Token", p.key)
+
+    rsp, err :=  http.DefaultClient.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    switch {
+        case rsp.StatusCode == 401:
+            return nil, fmt.Errorf("Missing of incorrect API key header")
+        case rsp.StatusCode == 422:
+            bytes,_:=ioutil.ReadAll(rsp.Body)
+            return nil, fmt.Errorf(string(bytes))
         case rsp.StatusCode == 500:
             return nil, fmt.Errorf("Postmark seems to be down!")
     }
